ef3fb569a4234c5e9b2f7a6c922781b8b94db09e
Mathew Ritchie <ritchmat89@gmail.com>
2025-11-09 19:05:33 +0200

    import amendment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import establishments from "./data/establishments";
+import establishments from "./data/establishments.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
